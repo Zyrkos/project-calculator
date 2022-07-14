1bd6d01cdefb1fbb0688dd488992f3a7362cbee4
Zyrkos <pedrocastro1995@hotmail.com>
2022-07-14 15:18:02 +0100

    .

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ class Calculator {
 
     }
 
-    del() {
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
 
     }
 
@@ -22,20 +23,73 @@ class Calculator {
     }
 
     chooseOperation(operation) {
+        if(this.currentOperand = '') return;
+        if(this.previousOperand !== '') {
+            this.compute();
+        };
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
 
     }
 
     compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '*':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
 
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalsDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+        if(isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0})
+        }
+        if(decimalsDigits != null) {
+            return `${integerDisplay}.${decimalsDigits}`
+        } else {
+            return integerDisplay
+        }
+
+        
     }
 
     updateDisplay () {
         this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
-
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)}
+            ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = '';
+        }
+       
     }
-
-
-    
 }
 
 const numberButtons = document.querySelectorAll('[data-number]');
@@ -56,6 +110,27 @@ numberButtons.forEach(button => {
     })
 })
 
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
 
 
 
